73bf218ee79f25843d693c1dcd9c9a9a223c7dec
snehal Jose <joyalsnehal3421@gmail.com>
2023-07-15 09:37:25 +0530

    Testing props

--- fetch-api/src/App.js
+++ fetch-api/src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [count,setCount]=useState(3);
+function handleClick(){
+  setCount(count+1);
+}
+function Button({count,onClick}){
+  return (
+    <button onClick={onClick}>
+      Cliked {count} times enjoy
+    </button>
+  )
+}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <Button count={count} onClick={handleClick}/>
+       
       </header>
     </div>
   );
