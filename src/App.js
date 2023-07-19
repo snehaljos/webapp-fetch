f740d0f41d94a843da1ba212f3d7584fc5dc0fca
snehal Jose <joyalsnehal3421@gmail.com>
2023-07-19 12:00:50 +0530

    Testing props and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import Button from './Components/Button.js';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Button from "./Components/Button.js";
+import Header from "./Components/Header";
 
 function App() {
-const [count,setCount]=useState(3);
-function handleClick(){
-  setCount(count+1);
-}
+  const [count, setCount] = useState(3);
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
-        <Button  onClick={handleClick} count={count}/>
-       
-      </header>
+      <Header />
+      <Button
+        style={{ padding: "20000" }}
+        onClick={handleClick}
+        count={count}
+      />
     </div>
   );
 }
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-
-function Button({onClick,count}) {
-    return(
-    <button className="btn btn-primary"  onClick={onClick}>
-      Cliked {count}      times enjoy
+function Button({ onClick, count }) {
+  return (
+    <button
+      className="btn btn-primary "
+      style={{ padding: "20000" }}
+      onClick={onClick}
+    >
+      Cliked {count} times enjoy
     </button>
-    );
+  );
 }
 
 export default Button;
