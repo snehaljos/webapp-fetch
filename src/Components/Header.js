d775163f7df9bfcc44d70eca2e9de053167e13d4
snehal Jose <joyalsnehal3421@gmail.com>
2023-08-23 09:40:05 +0530

    added more files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,50 +10,18 @@ function Header({ onClick, count }) {
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <Link to="Login"> rr</Link>
+          <li class="nav-item  ">
+            <Link class="nav-link" to="Login">
+              {" "}
+              Login
+            </Link>
           </li>
-          <li>
-            <Link class="nav-link" to="Home">
+          <li className={""}>
+            <Link class="nav-link " to="Home">
               Link
             </Link>
           </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">
-                Action
-              </a>
-              <a class="dropdown-item" href="#">
-                Another action
-              </a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">
-              Disabled
-            </a>
-          </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-            Search
-          </button>
-        </form>
       </div>
       <Outlet />
     </nav>
