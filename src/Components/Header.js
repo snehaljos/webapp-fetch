ecbf4d133bc933860b6c494e1bbc748164b7fa39
snehal Jose <joyalsnehal3421@gmail.com>
2023-12-19 10:45:07 +0530

    Updated UI foe signed in

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, Outlet } from "react-router-dom";
+import { isLogged } from "../Slice/loginSlice";
 
 function Header({ onClick, count }) {
+  const Logged=useSelector(isLogged);
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="Home">
         Navbar
       </Link>
-
+   
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
+          
           <li className="nav-item  ">
+          { !Logged &&
             <Link className="nav-link" to="Login">
               {" "}
               Login
             </Link>
+            }
+            { Logged &&
+            <Link className="nav-link" to="Success">
+              {" "}
+              Profile
+            </Link>
+            }
           </li>
           <li className={""}>
             <Link className="nav-link " to="Home">
--- src/Components/SuccessPage.js
+++ src/Components/SuccessPage.js
@@ -3,18 +3,21 @@ import { useDispatch } from "react-redux";
 import { loggedOut } from "../Slice/loginSlice";
 import photo from "../asset/profile.jpg"
 import "./SuccessPage.css";
+import { useNavigate } from "react-router-dom";
 
 function Success(){
     const dispatch=useDispatch();
+    const navigate=useNavigate();
     const handleClick = (e) =>{
-
-
+        e.preventDefault();
+        dispatch(loggedOut());
+        navigate("/Home");
     }
     return(
         <div className="Profile">
        <div className="Main">
         <img   className="Img"  src={photo} alt="SJ"/>
-        <div>Snehal Jose-SSE</div>
+        <div>Username-role</div>
        </div>
        
     <div className="Details">
@@ -27,7 +30,7 @@ function Success(){
 
     </div>
        
-        <button onClick={()=>dispatch(loggedOut())}>loggedOut</button>
+        <button onClick={handleClick}>loggedOut</button>
         </div>
     );
 }
