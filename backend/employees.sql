6ff7aeed185ff87d6180737ba60652451046abd3
snehal Jose <joyalsnehal3421@gmail.com>
2024-01-10 19:58:36 +0530

    added backend sql file

---
+++ backend/employees.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `employees` (
+    `employees_name` VARCHAR(8) CHARACTER SET utf8,
+    `employees_email` VARCHAR(22) CHARACTER SET utf8,
+    `employees_id` INT
+);
+INSERT INTO `employees` VALUES ('Shyam','shyamjaiswal@gmail.com',NULL),
+	('Bob','bob32@gmail.com',NULL),
+	('Jai','jai87@gmail.com',NULL),
+	('Shyam','shyamjaiswal@gmail.com',NULL),
+	('Bob','bob32@gmail.com',NULL),
+	('Jai','jai87@gmail.com',NULL),
+	('7','jai87@gmail.com',NULL),
+	('8','jai87@gmail.com',NULL),
+	('admin','jai87@gmail.com',NULL),
+	('adminsss','jai87@gmail.com',1);
